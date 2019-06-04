5dc4d140424978fa6ce73c26901f38bd52bcdecd
ChristianZ1234 <43790405+ChristianZ1234@users.noreply.github.com>
2019-06-04 14:19:03 -0700

    Add files via upload

---
+++ FriendBookGUI/src/sample/Controller.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+
+public class Controller {
+    public ListView showList;
+    public TextField textName;
+    public TextField textEmail;
+    public TextField textSchool;
+    public Label labelName;
+    public Label labelSchool;
+    public Label labelEmail;
+
+    public void addFriend(ActionEvent actionEvent) {
+        
+    }
+
+    public void deleteFriend(ActionEvent actionEvent) {
+    }
+}
